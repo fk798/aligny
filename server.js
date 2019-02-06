a9a37d8533f3198e139863b9be2edd972941d1c3
fk798 <fk798@nyu.edu>
2019-02-06 08:52:46 -0500

    dumb path

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const nodeMailer= require("nodemailer");
+const path = require('path');
 const app = express();
 const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
