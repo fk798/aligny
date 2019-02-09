aec127b24b79d6750e5b41f9ed5a055dc190dca0
fk798 <fk798@nyu.edu>
2019-02-09 14:24:29 -0500

    dumb edut

--- client/src/pages/Main/Main.js
+++ client/src/pages/Main/Main.js
@@ -5,7 +5,7 @@ class Main extends Component {
         return (
             <React.Fragment>
                 <h3>Mission Statement</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec enim odio, dictum ac quam a, tristique ultrices orci. Cras pellentesque dolor nec risus vulputate convallis. Aliquam elementum auctor ligula, in luctus nibh porta in. Etiam a molestie orci, tempus consequat mi. Nulla non tempus nunc. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Sed magna massa, lacinia et mauris quis, condimentum suscipit purus. Praesent auctor nibh lorem, ac posuere lorem fermentum fringilla. Cras ac felis sapien. Vestibulum laoreet aliquet mauris ut lacinia. Maecenas eget enim fermentum, posuere lacus nec, auctor odio. </p>
+                <p>Under Construction</p>
             </React.Fragment>
         );
     }
