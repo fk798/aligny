f79beaa64212973e198cdc79717a0de46419eec8
fk798 <fk798@nyu.edu>
2021-10-05 21:35:05 -0400

    added more pics

--- pages/gallery/2019.tsx
+++ pages/gallery/2019.tsx
@@ -8,11 +8,16 @@ import { Container } from "@mui/material";
 export default function _2019() {
 
     const images = [
+        { original: 'https://s3.amazonaws.com/aaany-assets/images/2019/BZDM9728.JPG' },
+        { original: 'https://s3.amazonaws.com/aaany-assets/images/2019/DNWK4528.JPG' },
         { original: 'https://s3.amazonaws.com/aaany-assets/images/2019/DYQK6403.JPG' },
+        { original: 'https://s3.amazonaws.com/aaany-assets/images/2019/FPEX4089.JPG' },
+        { original: 'https://s3.amazonaws.com/aaany-assets/images/2019/JNGY1199.JPG' },
         { original: 'https://s3.amazonaws.com/aaany-assets/images/2019/PDFV8152.JPG' },
         { original: 'https://s3.amazonaws.com/aaany-assets/images/2019/PDFV8535.JPG' },
         { original: 'https://s3.amazonaws.com/aaany-assets/images/2019/PDNQ9062.JPG' },
         { original: 'https://s3.amazonaws.com/aaany-assets/images/2019/RGMP9057.JPG' },
+        { original: 'https://s3.amazonaws.com/aaany-assets/images/2019/XBEG4348.JPG' },
         { original: 'https://s3.amazonaws.com/aaany-assets/images/2019/YJRF9607.JPG' },
         ]
 
