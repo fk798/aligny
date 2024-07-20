89ae2b50fc5868ac675c777d4142157335c19605
Faisal Karim <faisalkarim@Faisals-MBP.lan>
2024-07-20 14:08:15 -0400

    fixed urls of images

--- pages/gallery/2024/picnic.tsx
+++ pages/gallery/2024/picnic.tsx
@@ -11,10 +11,10 @@ export default function Picnic() {
     const images = [
         { original: 'https://aaany-assets.s3.amazonaws.com/2024/amu+picnic+2024/WhatsApp+Image+2024-07-19+at+10.53.21+AM.jpeg' },
         { original: 'https://aaany-assets.s3.amazonaws.com/2024/amu+picnic+2024/WhatsApp+Image+2024-07-19+at+10.53.22+AM+(1).jpeg' }, 
-        { original: 'https://aaany-assets.s3.amazonaws.com/2024/amu+picnic+2024/WhatsApp+Image+2024-07-19+at+10.53.22+AM+(1).jpeg' }, 
-        { original: 'https://aaany-assets.s3.amazonaws.com/2024/amu+picnic+2024/WhatsApp+Image+2024-07-19+at+10.53.22+AM+(1).jpeg' }, 
-        { original: 'https://aaany-assets.s3.amazonaws.com/2024/amu+picnic+2024/WhatsApp+Image+2024-07-19+at+10.53.22+AM+(1).jpeg' }, 
-        { original: 'https://aaany-assets.s3.amazonaws.com/2024/amu+picnic+2024/WhatsApp+Image+2024-07-19+at+10.53.22+AM+(1).jpeg' }, 
+        { original: 'https://aaany-assets.s3.amazonaws.com/2024/amu+picnic+2024/WhatsApp+Image+2024-07-19+at+10.53.22+AM+(2).jpeg' }, 
+        { original: 'https://aaany-assets.s3.amazonaws.com/2024/amu+picnic+2024/WhatsApp+Image+2024-07-19+at+10.53.22+AM+(3).jpeg' }, 
+        { original: 'https://aaany-assets.s3.amazonaws.com/2024/amu+picnic+2024/WhatsApp+Image+2024-07-19+at+10.53.22+AM+(4).jpeg' }, 
+        { original: 'https://aaany-assets.s3.amazonaws.com/2024/amu+picnic+2024/WhatsApp+Image+2024-07-19+at+10.53.22+AM+(5).jpeg' }, 
         { original: 'https://aaany-assets.s3.amazonaws.com/2024/amu+picnic+2024/WhatsApp+Image+2024-07-19+at+10.53.22+AM.jpeg' }, 
         { original: 'https://aaany-assets.s3.amazonaws.com/2024/amu+picnic+2024/WhatsApp+Image+2024-07-19+at+10.53.23+AM.jpeg' }, 
         ]
