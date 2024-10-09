5cce4cef1e07efa53a00de1e31297ae03031c447
Faisal Karim <fk798@nyu.edu>
2024-10-08 21:46:13 -0400

    added changes

--- pages/membership/index.tsx
+++ pages/membership/index.tsx
@@ -21,7 +21,7 @@ export default function Membership() {
           <h1>Become a Member!</h1>
         </div>
             <p>The Aligarh Alumni Association of New York is geared towards achieving the goals of Sir Syed Ahmad Khan such as education and economic development of under-privileged communities, emphasis on modern scientific knowledge and work towards unity and harmony among different communities. In order to achieve those goals AAANY welcomes everyone to join our organization!</p>
-            <p>To become a member, please fill out the form below and email us at <a href = "mailto:aaany1017@gmail.com">aaany1017@gmail.com</a>.</p>
+            <p>To become a member, please fill out the form below and email us at <a href = "mailto:aaanyteam@gmail.com">aaanyteam@gmail.com</a>.</p>
             <div style = {{textAlign: "center"}}>
             <object data = "https://aaany-assets.s3.amazonaws.com/2024/Membershi+Form+2024.pdf" width="800" height = "1000"></object>
             </div>
