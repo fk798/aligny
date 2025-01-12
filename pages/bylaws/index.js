f6e2207ec840a2e3d28dff5a197ec7301e4aa5cc
Faisal Karim <fk798@nyu.edu>
2025-01-12 18:37:05 -0500

    added const

--- pages/bylaws/index.js
+++ pages/bylaws/index.js
@@ -5,7 +5,7 @@ import Footer from '../../components/Footer'
 import { Container } from 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-export default function Constitution() {
+export default function Bylaws() {
   return (
     <Container maxWidth = "xl">
       <Head>
@@ -19,6 +19,11 @@ export default function Constitution() {
         Constitution and Bylaws of AAANY
         </h1>
         <div style = {{textAlign: "center"}}>
+            <h3>Constitution and Bylaws</h3>
+            <object data = "https://aaany-assets.s3.us-east-1.amazonaws.com/files/bylaws-min.pdf" width="800" height = "1000"></object>
+            </div>
+        <div style = {{textAlign: "center"}}>
+            <h3>Amendments</h3>
             <object data = "https://aaany-assets.s3.us-east-1.amazonaws.com/files/Amendments_to_the_Constitution_and_ByLaws_of_AAA_NYTri-State+2011.pdf" width="800" height = "1000"></object>
             </div>
       </main>
